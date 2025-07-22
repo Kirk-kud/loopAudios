7b07bd83219614754f8b811d0aa471251f1a4db0
Kirk Kudoto <135407806+Kirk-kud@users.noreply.github.com>
2025-07-22 16:59:43 +0000

    Cleaning up project

--- main.py
+++ main.py
@@ -4,6 +4,13 @@ from pydub import AudioSegment
 import questionary
 
 def loop_audio(audio_file, file_name, number_of_hours):
+    """
+    Function loops an audio file till it has reached the hour limit given by the user
+    :param audio_file: the AudioSegment object into which the audio has been loaded
+    :param file_name: name of the original audio file
+    :param number_of_hours: number of hours the looped audio must reach
+    :return: exported audio file
+    """
     if number_of_hours > 5:
         print("The limit for looping is 5 hours")
         return
@@ -17,6 +24,7 @@ def loop_audio(audio_file, file_name, number_of_hours):
 
     file_type = get_file_type()
 
+    # Working around the inherent m4a limitation in the ffmpeg library
     export_format = ""
     if file_type == "m4a":
         export_format = "ipod"
@@ -28,7 +36,13 @@ def loop_audio(audio_file, file_name, number_of_hours):
         format=export_format
     )
 
+
 def get_file_name(file_path):
+    """
+    Function gets the file name from the file path
+    :param file_path: relative path of the audio file
+    :return: the file name of the audio file, excluding the file type
+    """
     file_length = len(file_path)
     file_name = ""
 
@@ -41,17 +55,27 @@ def get_file_name(file_path):
     return (file_name[::-1])[:-4]
 
 def get_file_path():
+    """
+    Function gets the file path from the user using the questionary path input
+    :return: relative path of the audio file
+    """
     file = questionary.path(
         "What audio file would you like to loop?"
     ).ask()
 
+    # Checking that the file falls into the allowed audio formats
     if file.lower().endswith(".mp3") or file.lower().endswith(".wav") or file.lower().endswith(".m4a"):
         return file
     else:
         print("File type not supported.")
         sys.exit()
 
+
 def get_number_of_hours():
+    """
+    Function gets the number of hours the looped audio must reach
+    :return: the number of hours the looped audio must reach
+    """
     number_of_hours = questionary.select(
         "How many hours do you want to loop for?",
         choices=["1", "2", "3", "4", "5"]
@@ -67,6 +91,7 @@ def get_file_type():
 
     return choice
 
+
 def main():
     # Getting the audio file
     file = get_file_path()
@@ -79,6 +104,7 @@ def main():
 
     # Looping audio
     loop_audio(user_audio, file_name, loop_duration)
+
 
 if __name__ == "__main__":
     main()
